cf73e0add336bc3902c00dac8ff420cd1feeb3e9
Gautamaggrawal <gautamaggrawalsd@yahoo.in>
2019-09-30 04:10:16 +0530

    postgress config

--- run_web.sh
+++ run_web.sh
@@ -2,6 +2,12 @@
 
 # wait for PSQL server to start
 sleep 10
+docker-compose exec psql -U postgres -c "CREATE DATABASE postgres;"
+docker-compose exec psql -U postgres -c "CREATE USER myprojectuser WITH PASSWORD 'password';"
+docker-compose exec psql -U postgres -c "ALTER ROLE myprojectuser SET client_encoding TO 'utf8';"
+docker-compose exec psql -U postgres -c "ALTER ROLE myprojectuser SET default_transaction_isolation TO 'read committed';"
+docker-compose exec psql -U postgres -c "ALTER ROLE myprojectuser SET timezone TO 'UTC';"
+docker-compose exec psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE postgres TO myprojectuser;"
 cd backend  
 # prepare init migration
 su -m myuser -c "python3 manage.py makemigrations"  
