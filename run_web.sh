5711b56dd31998b0bd35c5f81b75d787b84ce084
Gautamaggrawal <gautamaggrawalsd@yahoo.in>
2019-09-30 03:13:04 +0530

    up

--- run_web.sh
+++ run_web.sh
@@ -5,9 +5,9 @@ sleep 10
 
 cd backend  
 # prepare init migration
-su -m myuser -c "python manage.py makemigrations"  
+su -m root -c "python manage.py makemigrations"  
 # migrate db, so we have the latest db schema
-su -m myuser -c "python manage.py migrate"  
+sudo -m root -c "python manage.py migrate"  
 # start development server on public ip interface, on port 8000
-su -m myuser -c "python manage.py runserver 0.0.0.0:8000"  
+su -m root -c "python manage.py runserver 0.0.0.0:8000"  
 
